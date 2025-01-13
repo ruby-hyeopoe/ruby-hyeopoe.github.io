5c4ad7be3884d2700ed2a601b07d2f4c2083a7ff
team-milestone-alfonso <alfonso@team-milestone.io>
2025-01-13 16:16:06 +0900

    dhh 의 rails8 소개 번역

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,4 +1,6 @@
 import "$styles/index.css"
+import "$styles/global.css"
+import "$styles/syntax.css"
 import "$styles/syntax-highlighting.css"
 
 // Import all JavaScript & CSS files from src/_components
